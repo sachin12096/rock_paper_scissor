18555be8ec5049c880a1021c68da77094b1bbcc8
unknown <sachin.shriwastri@gmail.com>
2023-06-07 23:19:40 +0530

    initil commit

---
+++ rockpapperscissors.py
@@ -0,0 +1,27 @@
+import random
+
+user_action=input("enter a choice(rock,paper,scissors)")
+
+possible=["rock","paper","scissor"]
+
+computer_action=random.choice(possible)
+
+print(f"\nyour choice{user_action},computer choice{computer_action}")
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
+                  
